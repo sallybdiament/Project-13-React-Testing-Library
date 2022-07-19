bee4a3a2132fb1f5134304c4040d3e16b915e845
Sally Diament <sally.barzilai@gmail.com>
2022-07-19 10:10:43 -0300

    incluindo o requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import { Pokedex } from '../pages';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+test('se a página contém um heading h2 com o texto Encountered pokémons', () => {
+  renderWithRouter(<App />);
+  const pokTitle = screen.getByRole('heading', { name: 'Encountered pokémons' });
+  expect(pokTitle).toBeInTheDocument();
+});
+
+test('é exibido o próximo pokémon quando o botão Próximo pokémon é clicado', () => {
+  renderWithRouter(<App />);
+  const btnNextPokemon = screen.getByTestId('next-pokemon');
+  expect(btnNextPokemon).toBeInTheDocument();
+  // userEvent.click(btnNextPokemon);
+});
