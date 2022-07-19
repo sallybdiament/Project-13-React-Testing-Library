2d3fd3160bb4e510b2def5d4dc1e108ac90b760f
Sally Diament <sally.barzilai@gmail.com>
2022-07-19 20:33:09 -0300

    incluindo o requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import App from '../App';
 
@@ -18,6 +19,10 @@ test('título summery e pikachu details deve aparecer na página ./pokemons/25',
   expect(titleDetail).toBeInTheDocument();
   const summaryTitle = screen.getByRole('heading', { name: 'Summary' });
   expect(summaryTitle).toBeInTheDocument();
+  const p = screen.getByText(/This intelligent Pokémon roasts/);
+  expect(p).toBeInTheDocument();
+  const locTitle = screen.getByRole('heading', { name: 'Game Locations of Pikachu' });
+  expect(locTitle).toBeInTheDocument();
   const image = screen.getAllByAltText('Pikachu location');
   expect(image[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
 });
@@ -25,6 +30,11 @@ test('título summery e pikachu details deve aparecer na página ./pokemons/25',
 test('O checkbox de favorito deve renderizar na página ./pokemons/25', () => {
   const { history } = renderWithRouter(<App />);
   history.push('/pokemons/25');
-  const labelCheckbox = screen.getByRole('checkbox');
+  const labelCheckbox = screen.getByText('Pokémon favoritado?');
   expect(labelCheckbox).toBeInTheDocument();
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).toBeInTheDocument();
+  userEvent.click(checkbox);
+  const image = screen.getByAltText('Pikachu is marked as favorite');
+  expect(image.src).toContain('/star-icon.svg');
 });
