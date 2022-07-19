a2ae4d10336ba32f017372b19d80c0a15f6967a1
Sally Diament <sally.barzilai@gmail.com>
2022-07-19 19:44:07 -0300

    incluindo o requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+test('título summery e pikachu details deve aparecer na página ./pokemons/25', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/pokemons/25');
+  const titleDetail = screen.getByRole('heading', { name: 'Pikachu Details' });
+  expect(titleDetail).toBeInTheDocument();
+  const summaryTitle = screen.getByRole('heading', { name: 'Summary' });
+  expect(summaryTitle).toBeInTheDocument();
+  const image = screen.getAllByAltText('Pikachu location');
+  expect(image[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+});
+
+test('O checkbox de favorito deve renderizar na página ./pokemons/25', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/pokemons/25');
+  const labelCheckbox = screen.getByRole('checkbox');
+  expect(labelCheckbox).toBeInTheDocument();
+});
