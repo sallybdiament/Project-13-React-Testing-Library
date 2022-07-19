b2069bf29941372442418451b6b2cdb0486c46fb
Sally Diament <sally.barzilai@gmail.com>
2022-07-18 22:22:21 -0300

    incluindo o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+test('se a página contém um heading h2 com o texto Page requested not found 😭', () => {
+  render(<NotFound />);
+  const notFoundTitle = screen.getByRole('heading', { name: /Page requested not/i });
+  expect(notFoundTitle).toBeInTheDocument();
+});
+
+test('se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  render(<NotFound />);
+  const img = 'Pikachu crying because the page requested was not found';
+  const image = screen.getByAltText(img);
+  expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
