ef8601e884b7da4cc90910da576ccd4bc0f1b96e
Sally Diament <sally.barzilai@gmail.com>
2022-07-19 15:40:31 -0300

    incluindo o requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import App from '../App';
@@ -26,4 +26,25 @@ test('é exibido o próximo pokémon quando o botão Próximo pokémon é clicad
   // userEvent.click(btnNextPokemon);
 });
 
-it('');
+it('se é mostrado apenas um pokémon por vez', () => {
+  renderWithRouter(<App />);
+  const pokemonName = screen.getAllByTestId('pokemon-name');
+  expect(pokemonName).toHaveLength(1);
+});
+
+it('Deve existir um botão de filtragem para cada tipo de pokémon, sem repetição', () => {
+  renderWithRouter(<App />);
+  const pokemonTipes = screen.getAllByTestId('pokemon-type-button');
+  const seven = 7;
+  expect(pokemonTipes).toHaveLength(seven);
+  const btnAll = screen.getByRole('button', { name: 'Electric' });
+  expect(btnAll).toBeInTheDocument();
+});
+
+it('tem um botão All', () => {
+  renderWithRouter(<App />);
+  const btnAll = screen.getByRole('button', { name: 'All' });
+  expect(btnAll).toBeInTheDocument();
+  userEvent.click(btnAll);
+
+});
